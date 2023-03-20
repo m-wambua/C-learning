7a9ccd1757e83ef3616d6327d546dee8b16fdfe6
m-wambua <michaelmutisyawambua@gmail.com>
2023-03-20 15:27:26 +0300

    full folder

---
+++ chapter-2/2.1C++programs/c++programs.cpp
@@ -0,0 +1,13 @@
+// the first simple program
+
+# include <iostream>
+int main()
+
+{
+std::cout<<"This is a simple program in C++";
+std::cout<<"to show the main structure."<<std::endl;
+std::cout<<"We learn more about the language";
+std::cout<<"in this chapter and the rest of the book";
+return 0;
+
+}
---
+++ chapter-2/2.1C++programs/c++programs_2.cpp
@@ -0,0 +1,18 @@
+/************************************************
+ * This program shows how we can print a square of asterisks.
+ * **************************************************
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+    //printing a square estrics
+    cout<<"******************"<<endl;
+    cout<<"******************"<<endl;
+    cout<<"******************"<<endl;
+    cout<<"******************"<<endl;
+    return 0;
+}
